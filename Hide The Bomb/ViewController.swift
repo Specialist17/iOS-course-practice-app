42e3771d4d40d5800369cc8ba1e7b886c87dc0d5
Specialist17 <Specialist@Fernandos-MacBook-Pro-2.local>
2015-11-07 15:05:26 -0400

    added files for hide the bomb app

--- Hide The Bomb/ViewController.swift
+++ Hide The Bomb/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blue: UIImageView!
+    @IBOutlet weak var red: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,35 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func hideBlue(sender: AnyObject) {
+        blue.hidden = true
+        red.hidden = false
+    }
+    @IBAction func hideRed(sender: AnyObject) {
+    
+        blue.hidden = false
+        red.hidden = true
+        
+    }
+    
+    
+    //Keeps the app GUI from rotating left or right
+    override func shouldAutorotate() -> Bool {
+        if (UIDevice.currentDevice().orientation == UIDeviceOrientation.LandscapeLeft ||
+            UIDevice.currentDevice().orientation == UIDeviceOrientation.LandscapeRight ||
+            UIDevice.currentDevice().orientation == UIDeviceOrientation.Unknown) {
+                return false
+        }
+        else {
+            return true
+        }
+    }
+    
+    //Permits the apps GUI to rotate vertically (regular vertical and upside down)
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return [UIInterfaceOrientationMask.Portrait ,UIInterfaceOrientationMask.PortraitUpsideDown]
     }
 
 
